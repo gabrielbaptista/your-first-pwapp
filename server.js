eb218e71ca2f2674efa9ad809d464ff52592e4ab
gabriell <gabriell@CE460>
2019-05-19 19:35:48 -0300

    Ajustes após tutorial Google

--- server.js
+++ server.js
@@ -21,12 +21,13 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+const config = require("./config.json");
 
 // CODELAB: Change this to add a delay (ms) before the server responds.
 const FORECAST_DELAY = 0;
 
 // CODELAB: If running locally, set your Dark Sky API key here
-const API_KEY = process.env.DARKSKY_API_KEY;
+const API_KEY = process.env.DARKSKY_API_KEY || config.DARKSKY_API_KEY;
 const BASE_URL = `https://api.darksky.net/forecast`;
 
 // Fake forecast data used if we can't reach the Dark Sky API
