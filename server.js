97ca0dd27a57b1d56b72a5d3029a5c91c3f096ed
gabriell <gabriell@CE460>
2019-05-19 19:49:38 -0300

    Ajuste da porta do site

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ function startServer() {
 
   // Handle requests for static files
   app.use(express.static('public'));
-
+  var apiPort = process.env.PORT || config.port;
   // Start the server
   return app.listen('8000', () => {
     // eslint-disable-next-line no-console
